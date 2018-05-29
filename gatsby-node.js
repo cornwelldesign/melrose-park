cb3f04ef5b9759960901095714cef290957a6f86
Tomomi Oki <tomomi.oki@cornwell.com.au>
2018-05-29 10:32:33 +1000

    Add template pages

---
+++ gatsby-node.js
@@ -0,0 +1,118 @@
+const _ = require(`lodash`)
+
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+   const { createPage } = boundActionCreators
+   return new Promise((resolve, reject) => {
+      // First, query all the pages on your WordPress
+      graphql(
+         `
+         {
+            allWordpressPage {
+              edges {
+                node {
+                  id
+                  slug
+                  template
+                  parent_element {
+                    id
+                    slug
+                  }
+                      acf {
+                        meta_title
+                        meta_description
+                        headline_1
+                        paragraph_1
+                        sub_title_1
+                        moving_text
+                        headline_2
+                        paragraph_2
+                        sub_title_2
+                        side_text
+                        header_1
+                        sub_header_1
+                        text_1
+                        sub_header_2
+                        text_2
+                        sub_header_3
+                        text_3
+                        header_2
+                        header_2_sub_text
+                        hero_text
+                        header
+                        main_paragraph
+                        sub_paragraph
+                        header_3
+                        paragraph_3
+                        header_4
+                        paragraph_4
+                        header_5
+                        paragraph_5
+                        header_6
+                        sub_title
+                        paragraph
+                        team_header
+                        team_1_name
+                        team_1_description
+                        team_2_name
+                        team_2_description
+                        team_3_name
+                        team_3_description
+                        slide_2_main_text
+                        slide_2_paragraph
+                        slide_2_link_text
+                        slide_3_main_text
+                        slide_3_paragraph
+                        slide_3_link_text
+                        slide_4_main_text
+                        slide_4_paragraph
+                        slide_4_link_text
+                        slide_5_main_text
+                        slide_5_paragraph
+                        slide_5_link_text
+                        slide_6_main_text
+                        slide_6_paragraph
+                        slide_6_link_text
+                        wordpress_
+                      }
+                  
+                }
+              }
+            }
+          }
+`          
+      ).then(result => {
+         if (result.errors) {
+            console.log(result.errors)
+            reject(result.errors)
+         }
+         _.each(result.data.allWordpressPage.edges, edge => {
+            var url
+            if (edge.node.parent_element) {
+               url = `${edge.node.parent_element.slug}/${edge.node.slug}`               
+            } else {
+               url = `${edge.node.slug}`
+            }
+            var template = `./src/templates/${edge.node.template.replace('.php', '')}.js`
+            createPage({
+               path: `/${url}/`,
+               component: slash(
+                  path.resolve(
+                     template
+                  )
+               ),
+               context: {
+                  id: edge.node.id,
+                  slug: edge.node.slug,
+                  parent_element: edge.node.id
+               }
+            })
+         })
+         resolve()
+      })
+      // === END TAGS ===
+   })
+}
---
+++ src/templates/ThankyouPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import GoogleMap from "../components/Map"
+import styled, { keyframes } from "styled-components";
+import * as vars from "../style/vars";
+import Image from "../components/Image.js";
+import Register from "../components/RegisterForm.js";
+import { H1 } from "../components/Text"
+
+class thanksPage extends React.Component {
+  componentDidMount() {
+    this.props.updateMeta(
+      "Thank You | Melrose Park ",
+      "Registration made."
+    );
+  }
+  render() {
+		
+		
+		return (
+			<ThanksSec>
+
+            <Image
+              src="/images/melrose-park-sydney-property-040-RESIDENTS.jpg"
+              alt="Melrose Park Property, Sydney - Group of residents"
+            />
+							<H1>THANK YOU<br/>FOR REGISTERING</H1>
+
+
+						</ThanksSec>
+
+
+    );
+  }
+}
+
+const ThanksSec = styled.div`
+height: 100vh;
+width: 100%;
+min-height: 60rem;
+position: relative;
+img{
+	position: absolute;
+	top: 0;
+	left: 0;
+	right:0;
+	bottom: 0;
+	z-index: 1;
+	margin: 0;
+	width: 100%;
+	height: 100%;
+	object-position: center;
+	object-fit: cover;
+}
+h1{
+	position: absolute;
+	left: 0;
+	right:0;
+  top: 50%;
+  transform: translateY(-50%);	margin: auto;
+	z-index: 2;
+	margin: auto;
+	color: ${vars.colors.blank};
+	text-shadow:rgba(0, 0, 0, 0.3) 0px 2px 32px;
+	text-align: center;
+}
+`
+
+
+
+
+
+export default thanksPage;
