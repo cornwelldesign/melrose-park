e6f7779be834a8b7fff2cb43524bf422f5f3c270
Jack Burns <jack.burns@cornwell.com.au>
2019-02-27 11:11:30 +1100

    console change

--- src/templates/PostPage.js
+++ src/templates/PostPage.js
@@ -13,7 +13,6 @@ class BusTimetable extends React.Component {
    }
    
    render() {
-		 console.log(this.props.data);
 		const wp = this.props.data.wordpressPost
 		return (
          <NewsWrapper>
